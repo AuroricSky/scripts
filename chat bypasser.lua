dcfa93b355048099832bcd6596691b19316c5d4c
duck0 <66388262+du3ko@users.noreply.github.com>
2020-12-16 15:25:20 -0500

    you say bad word in lego game XD

--- chat bypasser.lua
+++ chat bypasser.lua
@@ -1,11 +1,15 @@
 repeat wait() 
   until 
 game:IsLoaded()
+getgenv().Status = false
+if getgenv().Status == false then
+  game:GetService("StarterGui"):SetCore("SendNotification", {Title = "chat bypasser", Text = "currently not working, working on finding a new unicode character.", Duration = 5})
+elseif getgenv().Status == true then
 local meta, old = getrawmetatable(game), {}
   for i, v in next, meta do
   old[i] = v
 end
-local char = "أۘۘ" -- updating this every now and then.
+local char = "أۘۘ" -- updating this every now and then, currently trying to find a new one.
   setreadonly(meta, false)
     meta.__namecall =
   newcclosure(
@@ -56,27 +60,28 @@ local replacetext =
   "anal",
   "cock",
   "discord",
-  "condo"
+  "condo",
 }
 local replacedtext = ""
 if method == "FireServer" and args[1].Name == "SayMessageRequest" then
   for count = 1, #replacetext do
-     if string.find(args[2], replacetext[count]) then
-       replacedtext =
-          string.gsub(
-            replacetext[count],
-               ".",
-                  function(c)
-                    return c .. char
-                     end
-                    )
-                  args[2] = string.gsub(args[2], replacetext[count], replacedtext)
-               end
-            end
-          return old.__namecall(unpack(args))
-        end
+    if string.find(args[2], replacetext[count]) then
+      replacedtext =
+        string.gsub(
+          replacetext[count],
+            ".",
+              function(c)
+                return c .. char
+                  end
+                   )
+                 args[2] = string.gsub(args[2], replacetext[count], replacedtext)
+              end
+           end
+         return old.__namecall(unpack(args))
+       end
      return old.__namecall(...)
   end
 )
- setreadonly(meta, true)
-game:GetService("StarterGui"):SetCore("SendNotification", {Title = "chat bypasser", Text = "loaded. made by Cole#4200, modified by duck0#0415.", Duration = 5})
+setreadonly(meta, true)
+  game:GetService("StarterGui"):SetCore("SendNotification", {Title = "chat bypasser", Text = "loaded. made by Cole#4200, modified by duck0#0415.", Duration = 5})
+end
